20f4db8cfc8ad37721bca0c7ea9626fee74a7ab3
SHasan59 <121329046+SHasan59@users.noreply.github.com>
2024-01-29 22:32:50 -0500

    fixed typo

--- src/app/aboutus/page.tsx
+++ src/app/aboutus/page.tsx
@@ -1,11 +1,5 @@
 import AboutUsMain from "../components/aboutusmain";
 
-
-
-import AboutUsMain from "../components/aboutusmain";
-
-
-
 export default function About() {
   return (
    <AboutUsMain/>
