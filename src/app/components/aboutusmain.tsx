05f8930123a1f85bbc13bca64fecdcdaada6fd76
Samith Hasan <hasan.samith@email.com>
2024-01-29 20:48:39 -0500

    Updated about us page

--- src/app/components/aboutusmain.tsx
+++ src/app/components/aboutusmain.tsx
@@ -1,16 +1,36 @@
-
+import React from "react"
 
 export default function AboutUsMain() {
     return (
-      <main>
-  
+        <main>
         <title>About Us</title>
-   <div>about us page</div>
-   <h1>About Us</h1>
+        <div className="text-center">
+          <h1 className="text-blue-500 text-xl text-bold">About Us</h1>
   
-   <p>Our team has decided to create this because ....</p>
+          <br />
   
-   </main>
-    );
-  }
-  
+          <p>
+            Welcome to our About Us page. We are a passionate team dedicated
+            to providing an innovative way to organize and creating a positive impact in
+            our community.
+          </p>
+          <p>
+            Our mission is to make organization easier. We strive for
+            excellence in everything we do, and our commitment to quality is
+            unwavering.
+          </p>
+          <p>
+            Feel free to explore our website and learn more about what makes us unique.
+            If you have any questions or would like to get in touch, please don't
+            hesitate to contact us.
+          </p> 
+
+
+          <p>
+          Add team members info and pics
+          </p> 
+        </div>
+      </main>
+
+    )
+}
