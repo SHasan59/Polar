637f521cbf0c791a8576206a4cdabee50e148837
Samith Hasan <hasan.samith@email.com>
2024-01-31 14:42:05 -0500

    setup basic home page

---
+++ src/app/components/homemain.tsx
@@ -0,0 +1,34 @@
+import React from "react"
+
+export default function HomeMain() {
+    return (
+        <main>
+        <title>Home</title>
+        <div className="text-center">
+          <h1 className="text-blue-500 text-xl text-bold">Home</h1>
+  
+          <br />
+  
+          <p>
+            Welcome to our Home page. We are a passionate team dedicated
+            to providing an innovative way to organize and creating a positive impact in
+            our community.
+          </p>
+          <p>
+            Our mission is to make organization easier. We strive for
+            excellence in everything we do, and our commitment to quality is
+            unwavering.
+          </p>
+          <p>
+            Feel free to explore our website and learn more about what makes us unique.
+            If you have any questions or would like to get in touch, please don't
+            hesitate to contact us.
+          </p> 
+
+
+        <p>Add cards that might go to differnt pages</p> 
+        </div>
+      </main>
+
+    )
+}
