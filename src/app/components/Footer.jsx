6040ed452414add55a50b1b5cd63e2e35ab6ff45
Samith Hasan <hasan.samith@email.com>
2024-02-05 19:01:41 -0500

    fixed footer centering issue

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -4,16 +4,18 @@ import "../css/styles.css";
 
 const Footer = () => {
   return (
-    <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white text-xl">
-      <div className="container p-12 flex justify-between font-pixelify-sans">
+    <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white text-2xl">
+      <div className="container p-12 flex justify-between items-center font-pixelify-sans mx-auto">
         <span>Polar</span>
-        <div className="flex gap-8">
-          <Link href="/terms-of-service" className="text-slate-600 font-pixelify-sans hover:text-red-500">
-             Terms of Service
-          </Link>
-          <Link href="/privacy-policy" className="text-slate-600 font-pixelify-sans hover:text-red-500">
-          Privacy Policy
-          </Link>
+        <div className="justify-center">
+          <div className="flex gap-10">
+            <Link href="/terms-of-service" className="text-slate-600 font-pixelify-sans hover:text-red-500">
+              Terms of Service
+            </Link>
+            <Link href="/privacy-policy" className="text-slate-600 font-pixelify-sans hover:text-red-500">
+              Privacy Policy
+            </Link>
+          </div>
         </div>
         <p className="text-slate-600 font-pixelify-sans">© 2024</p>
       </div>
@@ -22,4 +24,5 @@ const Footer = () => {
 };
 
 export default Footer;
+
 
