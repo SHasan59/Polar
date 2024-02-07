88d269b4c3136ec926638d9d8dafb15de1061146
SHasan59 <121329046+SHasan59@users.noreply.github.com>
2024-02-06 19:18:15 -0500

    created basic chatbot

---
+++ src/app/chatbot/page.tsx
@@ -0,0 +1,16 @@
+import Navigation from '../components/Navigation';
+import Footer from '../components/Footer';
+import Chatbotmain from '../components/chatbotmain';
+
+export default function ChatBot() {
+  return (
+    <>
+      <>
+        <Navigation />
+        <Chatbotmain />
+      </>
+      <></>
+      <Footer />
+    </>
+  );
+}
