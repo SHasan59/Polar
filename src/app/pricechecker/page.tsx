54636eeec6c4287cc8d99fe0c78bde107f6fffc6
SHasan59 <121329046+SHasan59@users.noreply.github.com>
2024-02-18 20:27:13 -0500

    setup layout for price checker

---
+++ src/app/pricechecker/page.tsx
@@ -0,0 +1,19 @@
+import Footer from '../components/Footer';
+import Navigation from '../components/Navigation';
+import PriceChecker from '../components/pricecheckermain';
+
+export default function Home() {
+  return (
+    <main>
+      <>
+        <>
+          <Navigation />
+        </>
+        <PriceChecker />
+
+        <></>
+        <Footer />
+      </>
+    </main>
+  );
+}
