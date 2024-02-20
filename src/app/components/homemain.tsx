4930cc1fec162e44f55801e6ad595a5c9f39e4ce
SHasan59 <121329046+SHasan59@users.noreply.github.com>
2024-02-20 17:03:18 -0500

    Added particles to home page

--- src/app/components/homemain.tsx
+++ src/app/components/homemain.tsx
@@ -4,6 +4,7 @@ import Modal from '../components/Modal';
 import '../css/styles.css';
 import Link from 'next/link';
 import { Button } from './ui/moving-border';
+import { SparklesCore } from './ui/sparkles';
 
 const HomeMain = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -13,70 +14,72 @@ const HomeMain = () => {
   };
 
   return (
-    <main
-      className="bg-gray-700"
-      style={{
-        backgroundImage: "url('/background.jpg')",
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-      }}
-    >
-      <title>Home</title>
-      <div className="text-center">
-        <h1 className="text-blue-500 text-5xl font-bold font-orbitron">
-          Polar
-        </h1>
-
-        <br />
-
-        {/* Image */}
-
-        <Link href="/polarbearwalking">
-          <img
-            src="polarbear.webp"
-            alt="Polar Bear"
-            className="max-w-80 max-h-80 mx-auto mb-4 rounded-full"
+    <main>
+      <div className="h-[40rem] relative w-full bg-blue-900 flex flex-col items-center justify-center overflow-hidden ">
+        {/* Sparkles in the background */}
+        <div className="w-full absolute inset-0 h-screen">
+          <SparklesCore
+            id="tsparticlesfullpage"
+            background="transparent"
+            minSize={0.6}
+            maxSize={1.4}
+            particleDensity={100}
+            className="w-full h-full"
+            particleColor="#FFFFFF"
           />
-        </Link>
+        </div>
 
-        <p>
-          Welcome to our Home page. We are a passionate team dedicated to
-          providing an innovative way to organize and creating a positive impact
-          in our community.
-        </p>
+        {/* Content in front of sparkles */}
+        <div className="relative z-10">
+          <h1 className="text-blue-500 text-5xl font-bold font-orbitron text-center">
+            Polar
+          </h1>
 
-        <br />
+          <br />
 
-        <p>
-          Our mission is to make organization easier. We strive for excellence
-          in everything we do, and our commitment to quality is unwavering.
-        </p>
+          {/* Image */}
+          <Link href="/polarbearwalking">
+            <img
+              src="polarbear.webp"
+              alt="Polar Bear"
+              className="max-w-80 max-h-80 mx-auto mb-4 rounded-full cursor-pointer"
+            />
+          </Link>
 
-        <br />
+          <p>
+            Welcome to our Home page. We are a passionate team dedicated to
+            providing an innovative way to organize and creating a positive
+            impact in our community.
+          </p>
 
-        <p>
-          Feel free to explore our website and learn more about what makes us
-          unique. If you have any questions or would like to get in touch,
-          please don't hesitate to contact us.
-        </p>
+          <br />
 
-        {/* Rounded Buttons */}
-        <div className="flex justify-center mt-4">
-          <Button
-            className="bg-blue-500 text-white px-4 py-2 rounded-full mr-4"
-            onClick={handleToggleModal}
-          >
-            Search Games
-          </Button>
-          <button className="bg-blue-500 text-white px-4 py-2 rounded-full">
-            About Us
-          </button>
+          <p>
+            Our mission is to make organization easier. We strive for excellence
+            in everything we do, and our commitment to quality is unwavering.
+          </p>
+
+          <br />
+
+          <p>
+            Feel free to explore our website and learn more about what makes us
+            unique. If you have any questions or would like to get in touch,
+            please don't hesitate to contact us.
+          </p>
+
+          {/* Rounded Buttons */}
+          <div className="flex justify-center mt-4">
+            <Button className="bg-blue-500 text-white px-4 py-2 rounded-full mr-4">
+              <Link href="/search">Search Games</Link>
+            </Button>
+            <Link href="/aboutus">
+              <Button className="bg-blue-500 text-white px-4 py-2 rounded-full">
+                About Us
+              </Button>
+            </Link>
+          </div>
         </div>
       </div>
-      {isModalOpen && (
-        <Modal onClose={handleToggleModal}>Content of the modal</Modal>
-      )}
     </main>
   );
 };
