018ce39f058e9bcab162611ed41f7ba2f5d53ff0
Samith Hasan <121329046+SHasan59@users.noreply.github.com>
2024-02-21 16:07:00 -0500

    Revert "Fixed home ui & worked on added framer motion components (#8)" (#9)
    This reverts commit 8ff0c2ea51642ad25775e57fb1b053e73489b522.

--- src/app/components/homemain.tsx
+++ src/app/components/homemain.tsx
@@ -3,9 +3,6 @@ import React, { useState } from 'react';
 import Modal from '../components/Modal';
 import '../css/styles.css';
 import Link from 'next/link';
-import { Button } from './ui/moving-border';
-import { SparklesCore } from './ui/sparkles';
-import { TextGenerateEffect } from './ui/text-generate-effect';
 
 const HomeMain = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -13,67 +10,67 @@ const HomeMain = () => {
   const handleToggleModal = () => {
     setIsModalOpen(!isModalOpen);
   };
-  const words = ` Welcome to our Home page. We are a passionate team dedicated to
-  providing an innovative way to organize and creating a positive
-  impact in our community.
-  `;
-
-  const words2 = `  Our mission is to make organization easier. We strive for excellence
-  in everything we do, and our commitment to quality is unwavering.
-  `;
-
-  const words3 = `  Feel free to explore our website and learn more about what makes us
-  unique. If you have any questions or would like to get in touch,
-  please don't hesitate to contact us.
-  `;
 
   return (
-    <main>
-      <div className="h-[40rem] relative w-full bg-blue-900 flex flex-col items-center justify-center overflow-hidden ">
-        {/* Sparkles in the background */}
-        <div className="w-full absolute inset-0 h-screen">
-          <SparklesCore
-            id="tsparticlesfullpage"
-            background="transparent"
-            minSize={0.6}
-            maxSize={1.4}
-            particleDensity={100}
-            className="w-full h-full"
-            particleColor="#FFFFFF"
-          />
-        </div>
+    <main
+      className="bg-gray-700"
+      style={{
+        backgroundImage: "url('/background.jpg')",
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+      }}
+    >
+      <title>Home</title>
+      <div className="text-center">
+        <h1 className="text-blue-500 text-5xl font-bold font-orbitron">
+          Polar
+        </h1>
 
-        {/* Content in front of sparkles */}
-        <div className="relative z-10">
-          <h1 className="text-blue-500 text-5xl font-bold font-orbitron text-center">
-            Polar
-          </h1>
-          <br />
-          {/* Image */}
-          <Link href="/polarbearwalking">
-            <img
-              src="polarbear.webp"
-              alt="Polar Bear"
-              className="max-w-80 max-h-80 mx-auto mb-4 rounded-full cursor-pointer"
-            />
-          </Link>
-          <TextGenerateEffect className="text-white" words={words} />
-          <TextGenerateEffect words={words2} />
-          <TextGenerateEffect words={words3} />
-          {/* Rounded Buttons */}
-          <div className="flex justify-center mt-4">
-            <Button className="bg-slate-900 text-white  border-slate-800">
-              <Link href="/search">Search Games</Link>
-            </Button>
-            <Link href="/aboutus">
-              <Button
-                borderRadius="1.75rem"
-                className="bg-slate-900 text-white  border-slate-800"
-              >
-                About Us
-              </Button>
-            </Link>
-          </div>
+        <br />
+
+        {/* Image */}
+
+        <Link href="/polarbearwalking">
+          <img
+            src="polarbear.webp"
+            alt="Polar Bear"
+            className="max-w-80 max-h-80 mx-auto mb-4 rounded-full"
+          />
+        </Link>
+
+        <p>
+          Welcome to our Home page. We are a passionate team dedicated to
+          providing an innovative way to organize and creating a positive impact
+          in our community.
+        </p>
+
+        <br />
+
+        <p>
+          Our mission is to make organization easier. We strive for excellence
+          in everything we do, and our commitment to quality is unwavering.
+        </p>
+
+        <br />
+
+        <p>
+          Feel free to explore our website and learn more about what makes us
+          unique. If you have any questions or would like to get in touch,
+          please don't hesitate to contact us.
+        </p>
+
+        {/* Rounded Buttons */}
+        <div className="flex justify-center mt-4">
+          <button
+            className="bg-blue-500 text-white px-4 py-2 rounded-full mr-4"
+            onClick={handleToggleModal}
+          >
+            Search Games
+          </button>
+          <button className="bg-blue-500 text-white px-4 py-2 rounded-full">
+            About Us
+          </button>
         </div>
       </div>
       {isModalOpen && (
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,39 +1,20 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
-const colors = require('tailwindcss/colors');
-const {
-  default: flattenColorPalette,
-} = require('tailwindcss/lib/util/flattenColorPalette');
+import type { Config } from "tailwindcss";
 
-function addVariablesForColors({ addBase, theme }) {
-  let allColors = flattenColorPalette(theme('colors'));
-  let newVars = Object.fromEntries(
-    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
-  );
-
-  addBase({
-    ':root': newVars,
-  });
-}
-
-module.exports = {
+const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  darkMode: 'class',
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-      // You can add other theme extensions here if needed
     },
   },
-  plugins: [
-    addVariablesForColors,
-    // Add other plugins if needed
-  ],
+  plugins: [],
 };
+export default config;
