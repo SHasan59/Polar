8ff0c2ea51642ad25775e57fb1b053e73489b522
Samith Hasan <121329046+SHasan59@users.noreply.github.com>
2024-02-21 15:59:44 -0500

    Fixed home ui & worked on added framer motion components (#8)
    * updated dependency
    
    * fixed nav and added temp pixel art
    
    * fixed links for about page
    
    * Fix navigatio, added polar game, added images, fixed images and href for about us page, added modal component
    
    * Added Animated Button 😊also fixed ts config for easier plugins
    
    * Added particles to home page
    
    * added text animation to home page
    
    * added tracebeam for aboutus page
    
    * updated buttons

--- src/app/components/aboutusmain.tsx
+++ src/app/components/aboutusmain.tsx
@@ -1,6 +1,7 @@
 'use client';
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import { TracingBeam } from './ui/tracing-beam';
 
 const TeamMembers = [
   {
@@ -38,77 +39,110 @@ const AboutUsMain = () => {
       </Helmet>
 
       <main className="bg-gray-900 text-white min-h-screen">
-        <div className="container mx-auto py-12">
-          <div className="text-center">
-            <h1 className="text-4xl font-bold text-white-500 mb-6">About Us</h1>
+        <TracingBeam>
+          <div className="container mx-auto py-12">
+            <div className="text-center">
+              <h1 className="text-4xl font-bold text-white-500 mb-6">
+                About Us
+              </h1>
 
-            <div className="max-w-3xl mx-auto">
-              <p className="text-gray-400 text-lg mb-6">
-                Welcome to our About Us page. We are a passionate team dedicated
-                to providing an innovative way to organize and creating a
-                positive impact in our community. Our mission is to make
-                organization easier. We strive for excellence in everything we
-                do, and our commitment to quality is unwavering.
-              </p>
-            </div>
+              <div className="max-w-3xl mx-auto">
+                <p className="text-gray-400 text-lg mb-6">
+                  Welcome to our About Us page. We are a passionate team
+                  dedicated to providing an innovative way to organize and
+                  creating a positive impact in our community. Our mission is to
+                  make organization easier. We strive for excellence in
+                  everything we do, and our commitment to quality is unwavering.
+                </p>
+              </div>
 
-            <div className="mt-8 bg-gray-800 p-6 rounded-lg">
-              <h2 className="text-xl font-bold mb-4">Our Team</h2>
+              <div className="mt-8 bg-gray-800 p-6 rounded-lg">
+                <h2 className="text-xl font-bold mb-4">Our Team</h2>
 
-              {TeamMembers.map((member, index) => (
-                <div className="flex items-center mb-6" key={index}>
-                  <img
-                    src={member.image}
-                    alt={`${member.name} - ${member.role}`}
-                    className="w-12 h-12 rounded-full mr-4"
-                  />
-                  <div>
-                    <h3 className="text-white font-semibold text-lg">
-                      {member.name}
-                    </h3>
-                    <p className="text-gray-400">{member.role}</p>
-                    <div className="flex mt-2">
-                      <a
-                        href={member.linkedin}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <img
-                          src="/linkedin-icon.svg"
-                          alt="LinkedIn"
-                          className="w-6 h-6 mr-2"
-                        />
-                      </a>
-                      <a
-                        href={member.github}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <img
-                          src="/github-icon.svg"
-                          alt="GitHub"
-                          className="w-6 h-6"
-                        />
-                      </a>
+                {TeamMembers.map((member, index) => (
+                  <div className="flex items-center mb-6" key={index}>
+                    <img
+                      src={member.image}
+                      alt={`${member.name} - ${member.role}`}
+                      className="w-12 h-12 rounded-full mr-4"
+                    />
+                    <div>
+                      <h3 className="text-white font-semibold text-lg">
+                        {member.name}
+                      </h3>
+                      <p className="text-gray-400">{member.role}</p>
+                      <div className="flex mt-2">
+                        <a
+                          href={member.linkedin}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <img
+                            src="/linkedin-icon.svg"
+                            alt="LinkedIn"
+                            className="w-6 h-6 mr-2"
+                          />
+                        </a>
+                        <a
+                          href={member.github}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <img
+                            src="/github-icon.svg"
+                            alt="GitHub"
+                            className="w-6 h-6"
+                          />
+                        </a>
+                      </div>
                     </div>
                   </div>
-                </div>
-              ))}
+                ))}
+              </div>
+
+
+              <section className="my-8 bg-blue-800 p-6 rounded-lg">
+                <h2 className="text-2xl font-bold mb-4">Our Achievements</h2>
+                <p className="text-gray-400">PUT</p>
+
+                <p className="text-gray-400">ADD EMAIL</p>
+                <p className="text-gray-400">ADD EMAIL</p>
+                <p className="text-gray-400">ADD EMAIL</p>
+                <p className="text-gray-400">ADD EMAIL</p>
+                <p className="text-gray-400">ADD EMAIL</p>
+                <p className="text-gray-400">ADD EMAIL</p>
+                <p className="text-gray-400">ADD EMAIL</p>
+                <p className="text-gray-400 text-lg mb-6">
+                  Welcome to our About Us page. We are a passionate team
+                  dedicated to providing an innovative way to organize and
+                  creating a positive impact in our community. Our mission is to
+                  make organization easier. We strive for excellence in
+                  everything we do, and our commitment to quality is unwavering.
+                </p>
+                {/* Add more information about achievements */}
+              </section>
+
+              <section className="my-8 bg-green-800 p-6 rounded-lg">
+                <h2 className="text-2xl font-bold mb-4">Contact Us</h2>
+                <p className="text-gray-400">ADD EMAIL</p>
+                <p className="text-gray-400">ADD EMAIL</p>
+                <p className="text-gray-400">ADD EMAIL</p>
+                <p className="text-gray-400">ADD EMAIL</p>
+                <p className="text-gray-400">ADD EMAIL</p>
+                <p className="text-gray-400">ADD EMAIL</p>
+                <p className="text-gray-400 text-lg mb-6">
+                  Welcome to our About Us page. We are a passionate team
+                  dedicated to providing an innovative way to organize and
+                  creating a positive impact in our community. Our mission is to
+                  make organization easier. We strive for excellence in
+                  everything we do, and our commitment to quality is unwavering.
+                </p>
+                {/* Add your contact form here */}
+              </section>
             </div>
 
-            <section className="my-8 bg-blue-800 p-6 rounded-lg">
-              <h2 className="text-2xl font-bold mb-4">Our Achievements</h2>
-              <p className="text-gray-400">PUT</p>
-              {/* Add more information about achievements */}
-            </section>
-
-            <section className="my-8 bg-green-800 p-6 rounded-lg">
-              <h2 className="text-2xl font-bold mb-4">Contact Us</h2>
-              <p className="text-gray-400">ADD EMAIL</p>
-              {/* Add your contact form here */}
-            </section>
           </div>
-        </div>
+        </TracingBeam>
       </main>
     </>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,39 @@
-import type { Config } from "tailwindcss";
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
+const {
+  default: flattenColorPalette,
+} = require('tailwindcss/lib/util/flattenColorPalette');
 
-const config: Config = {
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme('colors'));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ':root': newVars,
+  });
+}
+
+module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode: 'class',
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      // You can add other theme extensions here if needed
     },
   },
-  plugins: [],
+  plugins: [
+    addVariablesForColors,
+    // Add other plugins if needed
+  ],
 };
-export default config;
