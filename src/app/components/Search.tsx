8028fb940fbb3c3b54240e9108f919b591268f08
SHasan59 <121329046+SHasan59@users.noreply.github.com>
2024-02-26 09:53:09 -0500

    added more filter options, adjusted framer motion🥶

--- src/app/components/Search.tsx
+++ src/app/components/Search.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { motion, Variants } from 'framer-motion';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import SearchBar from '../components/SearchBar';
@@ -15,15 +16,17 @@ interface Game {
 interface GameCardProps {
   game: Game;
 }
-
 const GameCard: React.FC<GameCardProps> = ({ game }) => {
   const defaultImage = '/beargame.png';
 
   return (
-    <div
+    <motion.div
       key={game.id}
+      initial={{ opacity: 0, x: -50 }}
+      animate={{ opacity: 1, x: 0 }}
+      transition={{ duration: 0.5 }}
       className="max-w-xs mx-auto bg-lightblue shadow-lg rounded-lg overflow-hidden m-4 h-96 overflow-y-auto"
-      style={{ backgroundColor: 'lightblue' }}
+      style={{ backgroundColor: 'lightblue', margin: '10px' }}
     >
       <img
         src={game.background_image || defaultImage}
@@ -41,11 +44,26 @@ const GameCard: React.FC<GameCardProps> = ({ game }) => {
           dangerouslySetInnerHTML={{ __html: game.about }}
         />
       </div>
-    </div>
+    </motion.div>
   );
 };
 
 export default function Search() {
+  const containerVariants: Variants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.1,
+      },
+    },
+  };
+
+  const gamesVariants: Variants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
+  };
+
   const [games, setGames] = useState<Game[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
   const [platformFilter, setPlatformFilter] = useState<string | null>(null);
@@ -114,18 +132,44 @@ export default function Search() {
           >
             <option value="">All Platforms</option>
             <option value="PC">PC</option>
-            <option value="Xbox">Xbox</option>
+            <option value="Web">Web</option>
+            <option value="Xbox One">Xbox One</option>
+            <option value="Xbox 360">Xbox 360</option>
+            <option value="Nintendo Switch">Nintendo Switch</option>
+            <option value="Wii U">Wii U</option>
+            <option value="Nintendo 3DS">Nintendo 3DS</option>
+            <option value="Wii">Wii</option>
+            <option value="Nintendo DS">Nintendo DS</option>
+            <option value="GameCube">GameCube</option>
+            <option value="Game Boy Advance">Game Boy Advance</option>
+            <option value="Nintendo 64">Nintendo 64</option>
+            <option value="SNES">SNES</option>
+            <option value="NES">NES</option>
+            <option value="PlayStation 5">PlayStation 5</option>
+            <option value="PlayStation 4">PlayStation 4</option>
+            <option value="PlayStation 3">PlayStation 3</option>
+            <option value="PlayStation 2">PlayStation 2</option>
+            <option value="PlayStation 1">PlayStation 1</option>
+            <option value="PS Vita">PS Vita</option>
+            <option value="PSP">PSP</option>
           </select>
         </div>
 
         {loading ? (
           <p className="text-center">Loading...</p>
         ) : (
-          <div className="flex flex-wrap justify-center overflow-y-auto max-h-screen">
+          <motion.div
+            variants={containerVariants}
+            initial="hidden"
+            animate="visible"
+            className="flex flex-wrap justify-center overflow-y-auto max-h-screen"
+          >
             {filteredGames.map((game) => (
-              <GameCard key={game.id} game={game} />
+              <motion.div key={game.id} variants={gamesVariants}>
+                <GameCard game={game} />
+              </motion.div>
             ))}
-          </div>
+          </motion.div>
         )}
       </div>
     </main>
