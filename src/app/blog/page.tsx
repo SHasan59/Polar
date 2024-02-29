f1332afe1d7a175f4c8ff1df310a46587bf24e3d
SHasan59 <121329046+SHasan59@users.noreply.github.com>
2024-02-29 13:19:18 -0500

    added bg customization

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,18 +1,20 @@
-import Head from 'next/head'
-import image from 'next/image'
-import Format from '../format/format'
-import Section1 from '../components/section1'
-import Section2 from '../components/section2'
-import Section3 from '../components/section3'
-import Section4 from '../components/section4'
+import Head from 'next/head';
+import image from 'next/image';
+import Format from '../format/format';
+import Section1 from '../components/section1';
+import Section2 from '../components/section2';
+import Section3 from '../components/section3';
+import Section4 from '../components/section4';
 
 export default function Blog() {
-    return (
-        <Format>
-            <Section1></Section1>
-            <Section2></Section2>
-            <Section3></Section3>
-            <Section4></Section4>
-        </Format>
-    )
+  return (
+    <div className="bg-black">
+      <Format>
+        <Section1></Section1>
+        <Section2></Section2>
+        <Section3></Section3>
+        <Section4></Section4>
+      </Format>
+    </div>
+  );
 }
