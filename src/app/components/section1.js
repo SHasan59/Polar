ec9401646f2acfa487eb9d94c97abd0ae6bc6993
Samith Hasan <hasan.samith@email.com>
2024-03-04 16:11:29 -0500

    Added more posts to blog page

--- src/app/components/section1.js
+++ src/app/components/section1.js
@@ -59,6 +59,26 @@ export default function section1() {
       content:
         'The Polar Bear Game" is an YESSSSS adventure set in the vast and unforgiving Arctic landscape, placing players in the furry paws of a courageous polar bear protagonist. As they navigate through icy terrain and face the harsh elements, players must overcome a series of challenges, from evading dangerous predators to solving puzzles and unraveling the mysteries of the Arctic wilderness. With stunning visuals and immersive storytelling, the game offers an unforgettable experience that not only entertains but also educates players about the importance of environmental conservation and the delicate balance of ecosystems. "The Polar Bear Game" promises an exhilarating journey where survival instincts and environmental awareness are key to success, inviting players to embark on an epic quest that celebrates the wonders of nature and the resilience of Arctic wildlife.',
     },
+    {
+      id: 5,
+      imageUrl: '/images/dragon.jpg',
+      title: 'The Polar Bear Game is the best game ever.',
+      linkUrl: '#',
+      category: 'Sub Heading',
+      date: 'March 1,2024',
+      content:
+        'The Polar Bear Game" is an YESSSSS adventure set in the vast and unforgiving Arctic landscape, placing players in the furry paws of a courageous polar bear protagonist. As they navigate through icy terrain and face the harsh elements, players must overcome a series of challenges, from evading dangerous predators to solving puzzles and unraveling the mysteries of the Arctic wilderness. With stunning visuals and immersive storytelling, the game offers an unforgettable experience that not only entertains but also educates players about the importance of environmental conservation and the delicate balance of ecosystems. "The Polar Bear Game" promises an exhilarating journey where survival instincts and environmental awareness are key to success, inviting players to embark on an epic quest that celebrates the wonders of nature and the resilience of Arctic wildlife.',
+    },
+    {
+      id: 6,
+      imageUrl: '/images/ssquad.png',
+      title: 'The Polar Bear Game is the best game ever.',
+      linkUrl: '#',
+      category: 'Sub Heading',
+      date: 'March 4,2024',
+      content:
+        'The Polar Bear Game" is an YESSSSS adventure set in the vast and unforgiving Arctic landscape, placing players in the furry paws of a courageous polar bear protagonist. As they navigate through icy terrain and face the harsh elements, players must overcome a series of challenges, from evading dangerous predators to solving puzzles and unraveling the mysteries of the Arctic wilderness. With stunning visuals and immersive storytelling, the game offers an unforgettable experience that not only entertains but also educates players about the importance of environmental conservation and the delicate balance of ecosystems. "The Polar Bear Game" promises an exhilarating journey where survival instincts and environmental awareness are key to success, inviting players to embark on an epic quest that celebrates the wonders of nature and the resilience of Arctic wildlife.',
+    },
     // more posts
   ];
 
