8858120e9bf06226c2e5fb6e3db26d620c69c725
SHasan59 <121329046+SHasan59@users.noreply.github.com>
2024-03-23 17:42:11 -0400

    updated navigation

--- src/app/components/Navigation.jsx
+++ src/app/components/Navigation.jsx
@@ -8,18 +8,6 @@ import { signOut, useSession } from 'next-auth/react';
 
 const navLinks = [
   {
-    title: 'Chatbot',
-    path: '/chatbot',
-  },
-  {
-    title: 'TaskList',
-    path: '/ToDo',
-  },
-  {
-    title: 'Search',
-    path: '/search',
-  },
-  {
     title: 'About Us',
     path: '/aboutus',
   },
@@ -27,10 +15,21 @@ const navLinks = [
     title: 'Blog',
     path: '/blog',
   },
-
   {
     title: 'Chat',
     path: '/chats',
+  },
+  {
+    title: 'Navi',
+    path: '/chatbot',
+  },
+  {
+    title: 'Search',
+    path: '/search',
+  },
+  {
+    title: 'Tasks',
+    path: '/ToDo',
   },
 ];
 
