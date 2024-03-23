b989738f8a02b42f23deef62a803d6c6d94289c7
SHasan59 <121329046+SHasan59@users.noreply.github.com>
2024-03-23 17:31:42 -0400

    cleanup of ui

--- src/app/components/ChatDetails.jsx
+++ src/app/components/ChatDetails.jsx
@@ -136,8 +136,8 @@ const ChatDetails = ({ chatId }) => {
           ) : (
             <>
               <img
-                src={otherMembers[0].profileImage || '/assets/person.jpg'}
-                alt="profile photo"
+                src={otherMembers[0].profileImage || '/person.jpg'}
+                alt="profile photo individual"
                 className=" w-11 h-11 rounded-full object-cover object-center"
               />
               <div className="text-white">
@@ -146,7 +146,7 @@ const ChatDetails = ({ chatId }) => {
             </>
           )}
         </div>
-        <div className=" flex-1 flex flex-col gap-5 bg-blue-200 p-5 overflow-y-scroll">
+        <div className=" flex-1 flex flex-col gap-5 bg-blue-300 p-5 overflow-y-scroll">
           {chat?.messages?.map((message, index) => (
             <MessageBox
               key={index}
