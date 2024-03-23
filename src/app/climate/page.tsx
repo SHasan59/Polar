12c9b44245a1c34e1f6c73fb5877514f464ac3a2
SHasan59 <121329046+SHasan59@users.noreply.github.com>
2024-03-23 02:10:00 -0400

    Setup Climate Page

---
+++ src/app/climate/page.tsx
@@ -0,0 +1,16 @@
+import Navigation from '../components/Navigation';
+import TabsDemo from '../components/feature';
+import Footer from '../components/Footer';
+
+export default function Climate() {
+  return (
+    <>
+      <>
+        <Navigation />
+        <TabsDemo />
+      </>
+      <></>
+      <Footer />
+    </>
+  );
+}
