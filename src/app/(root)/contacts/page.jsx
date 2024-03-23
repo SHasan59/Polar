f8352491135873dfc2e17d788f8673d2b7bd6f11
SHasan59 <121329046+SHasan59@users.noreply.github.com>
2024-03-23 01:31:14 -0400

    Added Nav and Footer to contact page

--- src/app/(root)/contacts/page.jsx
+++ src/app/(root)/contacts/page.jsx
@@ -1,10 +1,16 @@
 import Contacts from '../../components/Contacts';
+import Navigation from '../../components/Navigation';
+import Footer from '../../components/Footer';
 
 const ContactsPage = () => {
   return (
-    <div className="px-10 py-6 mb-20">
-      <Contacts />
-    </div>
+    <>
+      <Navigation />
+      <div className="px-10 py-6 mb-20">
+        <Contacts />
+      </div>
+      <Footer />
+    </>
   );
 };
 
