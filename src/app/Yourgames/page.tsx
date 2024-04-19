64caaebf7cb146ca7e4e5ef636d9d238c702fabe
GamalFares <112282940+GamalFares@users.noreply.github.com>
2024-04-19 04:02:42 +0000

    I am just a girl 🥺

---
+++ src/app/Yourgames/page.tsx
@@ -0,0 +1,21 @@
+import Footer from "../components/Footer";
+import Navigation from "../components/Navigation";
+import YourGame from "../components/yourgame";
+
+
+export default function Yourgame() {
+  return (
+  <main>
+
+<><><Navigation />
+      </>
+
+      <YourGame/>
+      <></>
+      <Footer /></>
+
+
+
+    </main>
+  );
+}
