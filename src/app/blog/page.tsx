07891224fa7b9205ca387284aa4696d8e1fc44c8
Muhammad Amir <umairamir1127@gmail.com>
2024-04-19 17:13:02 -0400

    Fixed Spacing

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -5,16 +5,13 @@ import Section1 from '../components/section1';
 import Section2 from '../components/section2';
 import Section4 from '../components/section4';
 
-
 export default function Blog() {
   return (
     <div className="bg-black">
       <Format>
-      <Section1></Section1>
+        <Section1></Section1>
         <Section2></Section2>
-         
         <Section4></Section4>
-       
       </Format>
     </div>
   )
