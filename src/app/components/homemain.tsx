64c562bc4c793de14cb589f68fd7bcd582f5d842
SHasan59 <121329046+SHasan59@users.noreply.github.com>
2024-04-20 13:35:34 -0400

    cleaned up old code, fixed words😊

--- src/app/components/homemain.tsx
+++ src/app/components/homemain.tsx
@@ -7,24 +7,6 @@ import { Button } from './ui/moving-border';
 import { SparklesCore } from './ui/sparkles';
 
 const HomeMain = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const handleToggleModal = () => {
-    setIsModalOpen(!isModalOpen);
-  };
-
-  const words = ` Welcome to our Home page. We are a passionate team dedicated to
-  providing an innovative way to organize and creating a positive
-  impact in our community.
-
-  Our mission is to make organization easier. We strive for excellence
-  in everything we do, and our commitment to quality is unwavering.
-
-  Feel free to explore our website and learn more about what makes us
-  unique. If you have any questions or would like to get in touch,
-  please don't hesitate to contact us.
-  `;
-
   return (
     <main>
       <div className="relative w-full bg-gray-900 flex flex-col items-center justify-center overflow-hidden ">
@@ -60,7 +42,20 @@ const HomeMain = () => {
               />
             </Link>
           </motion.div>
-          <p className="text-center mb-4">{words}</p>
+          <p className="text-center mb-2">
+            Welcome to our Home page. We are a passionate team dedicated to
+            providing an innovative way to organize and creating a positive
+            impact in our community.
+          </p>
+          <p className="text-center mb-2">
+            Our mission is to make organization easier. We strive for excellence
+            in everything we do, and our commitment to quality is unwavering.
+          </p>
+          <p className="text-center mb-10 text-m">
+            Feel free to explore our website and learn more about what makes us
+            unique. If you have any questions or would like to get in touch,
+            please don't hesitate to contact us.
+          </p>
 
           <div className="flex flex-col justify-center items-center space-y-4 md:flex-row md:justify-center md:space-x-4 md:space-y-0">
             <Button className="bg-blue-900 text-white  border-slate-800">
@@ -77,10 +72,6 @@ const HomeMain = () => {
           </div>
         </div>
       </div>
-
-      {isModalOpen && (
-        <Modal onClose={handleToggleModal}>Content of the modal</Modal>
-      )}
     </main>
   );
 };
