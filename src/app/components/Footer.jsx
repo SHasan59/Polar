ac52862f0e60d60070e4f126b19380ac6bc87c70
SHasan59 <121329046+SHasan59@users.noreply.github.com>
2024-04-21 06:22:30 -0400

    fixed footer layout

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -6,22 +6,25 @@ const Footer = () => {
   return (
     <footer className="footer border z-10 border-[#302f2f] border-l-transparent border-r-transparent text-white text-2xl">
       <div className="container p-12 flex flex-col sm:flex-row justify-between items-center font-pixelify-sans mx-auto">
-        <span className="text-2xl">Polar</span>
+        <span className="text-2xl mb-4 sm:mb-0">Polar</span>{' '}
+        {/* Added margin bottom for spacing */}
         <div className="flex flex-col sm:flex-row gap-2 sm:gap-10 items-center">
           <Link
             href="/termsofservice"
-            className="text-slate-600 font-pixelify-sans hover:text-blue-600 text-2xl"
+            className="text-slate-600 font-pixelify-sans hover:text-blue-600 text-2xl sm:text-center"
           >
             Terms of Service
           </Link>
           <Link
             href="/privacypolicy"
-            className="text-slate-600 font-pixelify-sans hover:text-blue-600 text-2xl"
+            className="text-slate-600 font-pixelify-sans hover:text-blue-600 text-2xl sm:text-center"
           >
             Privacy Policy
           </Link>
         </div>
-        <p className="text-white-600 font-pixelify-sans text-2xl">© 2024</p>
+        <p className="text-white-600 font-pixelify-sans text-2xl mt-4 sm:mt-0">
+          © 2024
+        </p>
       </div>
     </footer>
   );
