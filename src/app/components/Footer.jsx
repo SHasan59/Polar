a9ed899423dcc3ec88c4021935edf19d67929100
SHasan59 <121329046+SHasan59@users.noreply.github.com>
2024-04-21 06:06:41 -0400

    fixed footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -7,21 +7,19 @@ const Footer = () => {
     <footer className="footer border z-10 border-t-[#ffffff] border-l-transparent border-r-transparent text-white text-2xl">
       <div className="container p-12 flex flex-col sm:flex-row justify-between items-center font-pixelify-sans mx-auto">
         <span className="text-2xl">Polar</span>
-        <div className="justify-center">
-          <div className="flex flex-col sm:flex-row gap-2 sm:gap-10">
-            <Link
-              href="/termsofservice"
-              className="text-slate-600 font-pixelify-sans hover:text-blue-600 text-2xl sm:text-center"
-            >
-              Terms of Service
-            </Link>
-            <Link
-              href="/privacypolicy"
-              className="text-slate-600 font-pixelify-sans hover:text-blue-600 text-2xl sm:text-center"
-            >
-              Privacy Policy
-            </Link>
-          </div>
+        <div className="flex flex-col sm:flex-row gap-2 sm:gap-10 items-center">
+          <Link
+            href="/termsofservice"
+            className="text-slate-600 font-pixelify-sans hover:text-blue-600 text-2xl"
+          >
+            Terms of Service
+          </Link>
+          <Link
+            href="/privacypolicy"
+            className="text-slate-600 font-pixelify-sans hover:text-blue-600 text-2xl"
+          >
+            Privacy Policy
+          </Link>
         </div>
         <p className="text-white-600 font-pixelify-sans text-2xl">© 2024</p>
       </div>
