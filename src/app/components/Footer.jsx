0768043fa9df70f5d2ea77c948e0d31c76221229
SHasan59 <121329046+SHasan59@users.noreply.github.com>
2024-04-21 05:54:05 -0400

    fixed footer banner

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,23 +1,29 @@
 import Link from 'next/link';
-import React from "react";
-import "../css/styles.css";
+import React from 'react';
+import '../css/styles.css';
 
 const Footer = () => {
   return (
-    <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white text-2xl">
+    <footer className="footer border z-10 border-t-[#ffffff] border-l-transparent border-r-transparent text-white text-2xl">
       <div className="container p-12 flex flex-col sm:flex-row justify-between items-center font-pixelify-sans mx-auto">
-        <span className="text-lg sm:text-2xl lg:text-3xl">Polar</span>
+        <span className="text-2xl">Polar</span>
         <div className="justify-center">
           <div className="flex flex-col sm:flex-row gap-2 sm:gap-10">
-            <Link href="/termsofservice" className="text-slate-600 font-pixelify-sans hover:text-blue-600 text-md sm:text-lg lg:text-3xl">
+            <Link
+              href="/termsofservice"
+              className="text-slate-600 font-pixelify-sans hover:text-blue-600 text-2xl sm:text-center"
+            >
               Terms of Service
             </Link>
-            <Link href="/privacypolicy" className="text-slate-600 font-pixelify-sans hover:text-blue-600 text-md sm:text-lg lg:text-3xl">
+            <Link
+              href="/privacypolicy"
+              className="text-slate-600 font-pixelify-sans hover:text-blue-600 text-2xl sm:text-center"
+            >
               Privacy Policy
             </Link>
           </div>
         </div>
-        <p className="text-white-600 font-pixelify-sans text-md sm:text-xs lg:text-3xl">© 2024</p>
+        <p className="text-white-600 font-pixelify-sans text-2xl">© 2024</p>
       </div>
     </footer>
   );
