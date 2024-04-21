e62e570b73adbd491c4198d620b37040c55e54ba
SHasan59 <121329046+SHasan59@users.noreply.github.com>
2024-04-21 06:16:56 -0400

    fixed footer and nav border

--- src/app/components/Navigation.jsx
+++ src/app/components/Navigation.jsx
@@ -48,7 +48,7 @@ const Navigation = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
 
   return (
-    <nav className='"fixed top-0 left-0 right-0 z-10 bg-black-500 bg-opacity-100'>
+    <nav className='"fixed top-0 left-0 right-0 z-10 bg-black-500 bg-opacity-100 border  border-[#302f2f] border-l-transparent border-r-transparent'>
       <div className="flex flex-wrap items-center justify-between mx-auto px-4 py-2 ">
         <Link href={'/'}>
           <img
