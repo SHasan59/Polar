a17f32bd8353845c29aafb6697a293c9c50aa98b
Samith Hasan <121329046+SHasan59@users.noreply.github.com>
2024-04-21 06:26:48 -0400

    UI updates for nav and footer (#46)
    * incorporated loader to search
    
    * changed text color for better view
    
    * changed window size
    
    * fixed footer banner
    
    * fixed navigation
    
    * fixed footer
    
    * fixed footer and nav border
    
    * fixed footer layout

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,23 +1,30 @@
 import Link from 'next/link';
-import React from "react";
-import "../css/styles.css";
+import React from 'react';
+import '../css/styles.css';
 
 const Footer = () => {
   return (
-    <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white text-2xl">
+    <footer className="footer border z-10 border-[#302f2f] border-l-transparent border-r-transparent text-white text-2xl">
       <div className="container p-12 flex flex-col sm:flex-row justify-between items-center font-pixelify-sans mx-auto">
-        <span className="text-lg sm:text-2xl lg:text-3xl">Polar</span>
-        <div className="justify-center">
-          <div className="flex flex-col sm:flex-row gap-2 sm:gap-10">
-            <Link href="/termsofservice" className="text-slate-600 font-pixelify-sans hover:text-blue-600 text-md sm:text-lg lg:text-3xl">
-              Terms of Service
-            </Link>
-            <Link href="/privacypolicy" className="text-slate-600 font-pixelify-sans hover:text-blue-600 text-md sm:text-lg lg:text-3xl">
-              Privacy Policy
-            </Link>
-          </div>
+        <span className="text-2xl mb-4 sm:mb-0">Polar</span>{' '}
+        {/* Added margin bottom for spacing */}
+        <div className="flex flex-col sm:flex-row gap-2 sm:gap-10 items-center">
+          <Link
+            href="/termsofservice"
+            className="text-slate-600 font-pixelify-sans hover:text-blue-600 text-2xl sm:text-center"
+          >
+            Terms of Service
+          </Link>
+          <Link
+            href="/privacypolicy"
+            className="text-slate-600 font-pixelify-sans hover:text-blue-600 text-2xl sm:text-center"
+          >
+            Privacy Policy
+          </Link>
         </div>
-        <p className="text-white-600 font-pixelify-sans text-md sm:text-xs lg:text-3xl">© 2024</p>
+        <p className="text-white-600 font-pixelify-sans text-2xl mt-4 sm:mt-0">
+          © 2024
+        </p>
       </div>
     </footer>
   );
--- src/app/components/Navigation.jsx
+++ src/app/components/Navigation.jsx
@@ -48,8 +48,8 @@ const Navigation = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
 
   return (
-    <nav className='"fixed top-0 left-0 right-0 z-10 bg-black-500 bg-opacity-100'>
-      <div className="flex flex-wrap items-center justify-between mx-auto px-4py-2 ">
+    <nav className='"fixed top-0 left-0 right-0 z-10 bg-black-500 bg-opacity-100 border  border-[#302f2f] border-l-transparent border-r-transparent'>
+      <div className="flex flex-wrap items-center justify-between mx-auto px-4 py-2 ">
         <Link href={'/'}>
           <img
             src="/polar.png"
@@ -61,21 +61,21 @@ const Navigation = () => {
           {!navbarOpen ? (
             <button
               onClick={() => setNavbarOpen(true)}
-              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
+              className="flex items-center px-3 py-2 border rounded border-slate-200 text-blue-300 hover:text-blue-300 hover:border-white "
             >
               <Bars3Icon className="h-5 w-5" />
             </button>
           ) : (
             <button
               onClick={() => setNavbarOpen(false)}
-              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
+              className="flex items-center px-3 py-2 border rounded border-slate-200 text-blue-300 hover:text-blue-300 hover:border-white"
             >
               <XMarkIcon className="h-5 w-5" />
             </button>
           )}
         </div>
         <div className="menu hidden md:block md:w-auto" id="navbar">
-          <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
+          <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0 ">
             {navLinks.map((link, index) => (
               <li key={index}>
                 <NavigationLink href={link.path} title={link.title} />
