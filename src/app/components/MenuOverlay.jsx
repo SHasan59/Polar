d1ede3ed3564bb2a8441a1b382d1654cbb1194eb
SHasan59 <121329046+SHasan59@users.noreply.github.com>
2024-04-22 13:54:58 -0400

    adjusted size

--- src/app/components/MenuOverlay.jsx
+++ src/app/components/MenuOverlay.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const MenuOverlay = ({ links }) => {
   return (
-    <ul className="flex flex-col py-4 text-center text-2xl">
+    <ul className="flex flex-col py-4 text-center text-xl">
       {links.map((link, index) => (
         <li key={index}>
           <NavigationLink href={link.path} title={link.title} />
