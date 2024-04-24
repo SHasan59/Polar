f2b7654cd8aae3e3d9f3aba591840af5e37348ad
SHasan59 <121329046+SHasan59@users.noreply.github.com>
2024-04-24 00:11:54 -0400

    fixed description

--- src/app/components/feature.tsx
+++ src/app/components/feature.tsx
@@ -11,6 +11,7 @@ export default function TabsDemo() {
       content: (
         <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-blue-900 to-blue-200">
           <p>Chatbot</p>
+          <p>hello</p>
           <DummyContent />
         </div>
       ),
@@ -59,17 +60,17 @@ export default function TabsDemo() {
 
   return (
     <>
-      <div className="flex items-center justify-center flex-col my-40">
-        <h2 className="text-4xl font-bold mb-8">Features</h2>
+      <div className="flex items-center justify-center flex-col my-20">
+        <h2 className="text-5xl font-bold mb-8">Features</h2>
         <p className="mt-8 text-lg text-center">
-          ADD A DESCRIPTION TO THIShfeohdsifkhjdsihjvsdiklvjs;
-          sdijkvsdlkvjsdsckvjdkvjdoivjdovjodsjdiovsd
-          dcisdvnsdvsdivisvhuewfiehvuhkdxmc,mdcdmcdckdvbiuebgrsdh
-          8ggwr7rwejfoueyfiejfgfuwygdmwiwufie[pwufuiesdufipe'ifedifoeidopfed]
+          Welcome to our feature page, where excitement meets exploration! This
+          is the place to discover the details about Polar, There are many
+          features to explore. Click on the titles to go through each card and
+          look at each unqiue component.
         </p>
         <br></br>
         <br></br>
-        <div className="h-[20rem] md:h-[40rem] [perspective:1000px] relative b flex flex-col max-w-5xl mx-auto w-full items-start justify-start">
+        <div className="h-[20rem] md:h-[40rem] [perspective:1000px] relative b flex flex-col max-w-5xl mx-auto w-full items-start justify-start mb-20">
           <Tabs tabs={tabs} />
         </div>
       </div>
