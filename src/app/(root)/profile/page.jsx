0be01f5eb6cd69da7ca77d6c39b765b6695124da
SHasan59 <121329046+SHasan59@users.noreply.github.com>
2024-04-28 00:00:43 -0400

    added new upload profile key (fixed sign -> unsigned)

--- src/app/(root)/profile/page.jsx
+++ src/app/(root)/profile/page.jsx
@@ -99,7 +99,7 @@ const Profile = () => {
             <CldUploadButton
               options={{ maxFiles: 1 }}
               onUpload={uploadPhoto}
-              uploadPreset="upecg01j"
+              uploadPreset="p8icfni4"
             >
               <p className="text-body-bold">Upload new photo</p>
             </CldUploadButton>
