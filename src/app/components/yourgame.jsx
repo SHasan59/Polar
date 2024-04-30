dbd2b3ef3a7632cedb93ea2c0fc06d46fa41ef9e
GamalFares <112282940+GamalFares@users.noreply.github.com>
2024-04-30 01:10:16 +0000

    game collection fix

--- src/app/components/yourgame.jsx
+++ src/app/components/yourgame.jsx
@@ -1,10 +1,16 @@
 'use client';
 
 import React, { useState } from 'react';
-
 const PLATFORMS = ['PC', 'PlayStation', 'Xbox', 'Nintendo Switch'];
 const GAME_TYPES = ['Single Player', 'Multiplayer'];
 const GENRES = ['Action', 'Adventure', 'RPG', 'Shooter', 'Sports', 'Strategy', 'Simulation', 'Puzzle', 'Other'];
+
+const platformColors = {
+    'PC': '#1abc9c',         
+    'PlayStation': '#3498db', 
+    'Xbox': '#27ae60',       
+    'Nintendo Switch': '#e74c3c' 
+};
 
 export default function GameCollection() {
     const [gameCollection, setGameCollection] = useState({});
@@ -90,7 +96,6 @@ export default function GameCollection() {
                 setGameCollection((prevCollection) => {
                     const updatedCollection = { ...prevCollection };
 
-                    // Merge the imported data with the existing collection
                     for (const genre in importedData) {
                         if (!updatedCollection[genre]) {
                             updatedCollection[genre] = new Set();
@@ -138,162 +143,177 @@ export default function GameCollection() {
     };
 
     return (
-        <div className="max-w-lg mx-auto p-6 bg-black rounded-lg shadow-lg">
-            <h1 className="text-3xl font-bold mb-6 text-center text-white">Game Collection</h1>
+        <div className="min-h-screen bg-cover bg-center bg-auto" style={{ backgroundImage: "url('/images/gameshelf.png')" }}>
+            <div className="max-w-lg mx-auto p-6 bg-black rounded-lg shadow-lg bg-opacity-90">
+                <h1 className="text-3xl font-bold mb-6 text-center text-white">Game Collection</h1>
 
-            <div className="mb-6">
-                <div className="mb-2">
+                {/* Game form */}
+                <div className="mb-6">
+                    {/* Game name input */}
+                    <div className="mb-2">
+                        <input
+                            type="text"
+                            name="name"
+                            value={newGame.name}
+                            onChange={handleChange}
+                            placeholder="New Game"
+                            className="w-full p-2 border border-gray-300 rounded"
+                            style={{ color: 'black' }}
+                        />
+                    </div>
+
+                    {/* Genre selection */}
+                    <div className="mb-2">
+                        <select
+                            name="genre"
+                            value={newGame.genre}
+                            onChange={handleChange}
+                            className="w-full p-2 border border-gray-300 rounded"
+                            style={{ color: 'black' }}
+                        >
+                            <option value="" disabled>Select Genre</option>
+                            {GENRES.map((genre) => (
+                                <option key={genre} value={genre}>{genre}</option>
+                            ))}
+                        </select>
+                    </div>
+
+                    {/* Platform selection */}
+                    <div className="mb-2">
+                        <select
+                            name="platform"
+                            value={newGame.platform}
+                            onChange={handleChange}
+                            className="w-full p-2 border border-gray-300 rounded"
+                            style={{ color: 'black' }}
+                        >
+                            <option value="" disabled>Select Platform</option>
+                            {PLATFORMS.map((platform) => (
+                                <option key={platform} value={platform}>{platform}</option>
+                            ))}
+                        </select>
+                    </div>
+
+                    {/* Game type selection */}
+                    <div className="mb-2">
+                        <select
+                            name="type"
+                            value={newGame.type}
+                            onChange={handleChange}
+                            className="w-full p-2 border border-gray-300 rounded"
+                            style={{ color: 'black' }}
+                        >
+                            <option value="" disabled>Select Game Type</option>
+                            {GAME_TYPES.map((type) => (
+                                <option key={type} value={type}>{type}</option>
+                            ))}
+                        </select>
+                    </div>
+
+                    {/* Import game image button */}
+                    <button
+                        onClick={() => document.getElementById('image-import-input').click()}
+                        className="w-full bg-blue-500 text-white p-2 rounded mb-2"
+                    >
+                        Import Game Image
+                    </button>
                     <input
-                        type="text"
-                        name="name"
-                        value={newGame.name}
-                        onChange={handleChange}
-                        placeholder="New Game"
-                        className="w-full p-2 border border-gray-300 rounded"
-                        style={{ color: 'black' }}
+                        id="image-import-input"
+                        type="file"
+                        name="image"
+                        accept="image/*"
+                        onChange={handleImageImport}
+                        className="hidden"
+                    />
+
+                    {/* Add game button */}
+                    <button
+                        onClick={addGame}
+                        className="w-full bg-blue-500 text-white p-2 rounded"
+                    >
+                        Add Game
+                    </button>
+                </div>
+
+                {/* Export and import buttons */}
+                <div className="mb-6">
+                    {/* Export collection button */}
+                    <button
+                        onClick={exportCollection}
+                        className="w-full bg-green-500 text-white p-2 rounded"
+                    >
+                        Export Collection
+                    </button>
+
+                    {/* Import collection button */}
+                    <button
+                        onClick={() => document.getElementById('import-input').click()}
+                        className="w-full bg-yellow-500 text-white p-2 rounded mt-4"
+                    >
+                        Import Collection
+                    </button>
+                    <input
+                        id="import-input"
+                        type="file"
+                        accept=".json"
+                        onChange={importCollection}
+                        className="hidden"
                     />
                 </div>
 
-                <div className="mb-2">
-                    <select
-                        name="genre"
-                        value={newGame.genre}
-                        onChange={handleChange}
-                        className="w-full p-2 border border-gray-300 rounded"
-                        style={{ color: 'black' }}
-                    >
-                        <option value="" disabled>Select Genre</option>
-                        {GENRES.map((genre) => (
-                            <option key={genre} value={genre}>{genre}</option>
-                        ))}
-                    </select>
-                </div>
-
-                <div className="mb-2">
-                    <select
-                        name="platform"
-                        value={newGame.platform}
-                        onChange={handleChange}
-                        className="w-full p-2 border border-gray-300 rounded"
-                        style={{ color: 'black' }}
-                    >
-                        <option value="" disabled>Select Platform</option>
-                        {PLATFORMS.map((platform) => (
-                            <option key={platform} value={platform}>{platform}</option>
-                        ))}
-                    </select>
-                </div>
-
-                <div className="mb-2">
-                    <select
-                        name="type"
-                        value={newGame.type}
-                        onChange={handleChange}
-                        className="w-full p-2 border border-gray-300 rounded"
-                        style={{ color: 'black' }}
-                    >
-                        <option value="" disabled>Select Game Type</option>
-                        {GAME_TYPES.map((type) => (
-                            <option key={type} value={type}>{type}</option>
-                        ))}
-                    </select>
-                </div>
-
-                <button
-                    onClick={() => document.getElementById('image-import-input').click()}
-                    className="w-full bg-blue-500 text-white p-2 rounded mb-2"
-                >
-                    Import Game Image
-                </button>
-                <input
-                    id="image-import-input"
-                    type="file"
-                    name="image"
-                    accept="image/*"
-                    onChange={handleImageImport}
-                    className="hidden"
-                />
-
-                <button
-                    onClick={addGame}
-                    className="w-full bg-blue-500 text-white p-2 rounded"
-                >
-                    Add Game
-                </button>
-            </div>
-
-            <div className="mb-6">
-                <button
-                    onClick={exportCollection}
-                    className="w-full bg-green-500 text-white p-2 rounded"
-                >
-                    Export Collection
-                </button>
-
-                <button
-                    onClick={() => document.getElementById('import-input').click()}
-                    className="w-full bg-yellow-500 text-white p-2 rounded mt-4"
-                >
-                    Import Collection
-                </button>
-                <input
-                    id="import-input"
-                    type="file"
-                    accept=".json"
-                    onChange={importCollection}
-                    className="hidden"
-                />
-            </div>
-
-            {Object.keys(gameCollection).length > 0 && (
-                <div>
-                    <h2 className="text-xl font-semibold mb-4 text-white">Genres</h2>
-                    <table className="w-full text-white">
-                        <thead>
-                            <tr>
-                                <th className="border p-2">Game</th>
-                                <th className="border p-2">Platform</th>
-                                <th className="border p-2">Type</th>
-                                <th className="border p-2">Image</th>
-                                <th className="border p-2">Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {Object.keys(gameCollection).map((genre) => (
-                                <React.Fragment key={genre}>
-                                    <tr className="bg-gray-700">
-                                        <td colSpan={5} className="border p-2 font-semibold">{genre}</td>
-                                    </tr>
-                                    {[...gameCollection[genre]].map((game) => (
-                                        <tr key={game.name}>
-                                            <td className="border p-2">{game.name}</td>
-                                            <td className="border p-2">{game.platform}</td>
-                                            <td className="border p-2">{game.type}</td>
-                                            <td className="border p-2">
-                                                {game.imageUrl && (
-                                                    <img
-                                                        src={game.imageUrl}
-                                                        alt={game.name}
-                                                        className="w-16 h-16 object-cover rounded"
-                                                    />
-                                                )}
-                                            </td>
-                                            <td className="border p-2">
-                                                <button
-                                                    onClick={() => removeGame(genre, game)}
-                                                    className="bg-red-500 text-white p-2 rounded"
-                                                >
-                                                    Remove
-                                                </button>
-                                            </td>
+                {/* Game collection display */}
+                {Object.keys(gameCollection).length > 0 && (
+                    <div>
+                        <h2 className="text-xl font-semibold mb-4 text-white">Genres</h2>
+                        <table className="w-full text-white">
+                            <thead>
+                                <tr>
+                                    <th className="border p-2">Game</th>
+                                    <th className="border p-2">Platform</th>
+                                    <th className="border p-2">Type</th>
+                                    <th className="border p-2">Image</th>
+                                    <th className="border p-2">Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {Object.keys(gameCollection).map((genre) => (
+                                    <React.Fragment key={genre}>
+                                        <tr className="bg-gray-700">
+                                            <td colSpan={5} className="border p-2 font-semibold">{genre}</td>
                                         </tr>
-                                    ))}
-                                </React.Fragment>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
-            )}
+                                        {[...gameCollection[genre]].map((game) => (
+                                            <tr key={game.name}>
+                                                <td className="border p-2">{game.name}</td>
+                                                <td className="border p-2" style={{ color: platformColors[game.platform] }}>
+                                                    {game.platform}
+                                                </td>
+                                                <td className="border p-2">{game.type}</td>
+                                                <td className="border p-2">
+                                                    {game.imageUrl && (
+                                                        <img
+                                                            src={game.imageUrl}
+                                                            alt={game.name}
+                                                            className="w-16 h-16 object-cover rounded"
+                                                        />
+                                                    )}
+                                                </td>
+                                                <td className="border p-2">
+                                                    <button
+                                                        onClick={() => removeGame(genre, game)}
+                                                        className="bg-red-500 text-white p-2 rounded"
+                                                    >
+                                                        Remove
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                        ))}
+                                    </React.Fragment>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
